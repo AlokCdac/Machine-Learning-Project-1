7bdeb69807c1c923f399ab9c19ea7e375b6c840d
AlokCdac <bond.president@gmail.com>
2022-08-02 03:42:47 +0530

    exception and logger created

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
+
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 
 def index():
+    try:
+        raise Exception('we are testing our exception class')
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info('we are testing logging module ')
     return "This is my flask app aur abhi kam chalu hai "
+    
+        
+        
+
+    
+    
 
 
 if __name__ == '__main__':
